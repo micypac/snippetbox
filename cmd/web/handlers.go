8b5dd9e0d1a9affb388dc19e9b612779920a7bc5
Michael Pacleb <michael.pacleb@gmail.com>
2024-05-26 16:11:29 -0700

    implemented validator package with rules and logic that can be reused across the app and can easily extended to include additional rules.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,9 +7,18 @@ import (
 	"strconv"
 
 	"snippetbox.micypac.io/internal/models"
+	"snippetbox.micypac.io/internal/validator"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+type snippetCreateForm struct {
+	Title string
+	Content string
+	Expires int
+	validator.Validator
+}
+
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	
@@ -78,6 +87,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 
+	// Initialize a new snippetCreateForm instance and pass to template.
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
+
 	app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
 
@@ -89,15 +103,32 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
+
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	form := snippetCreateForm{
+		Title: r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
+	}
+
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must be equal 1, 7 or 365")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
+		return
+	}
+
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
